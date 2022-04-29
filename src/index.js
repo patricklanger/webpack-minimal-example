3fabbfb93f4a9cdbdd3fd04357bc8ea1d1f27f58
patricklanger <84206409+patricklanger@users.noreply.github.com>
2022-04-29 19:36:11 +0200

    add comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./App.js";
+
+// Create our react root
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
 
+// Add a h1 to the html file with common JS
 document.addEventListener("DOMContentLoaded", function(event) {
     const element = document.createElement('h1')
     element.innerHTML = "And I am vanillaJS!"
